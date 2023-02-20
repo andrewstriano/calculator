0e66d074b2c8dd6bdb3d71bb88393ec5163be961
andrewstriano <111657219+andrewstriano@users.noreply.github.com>
2023-02-19 19:05:56 -0800

    Refactor and lint

--- main.js
+++ main.js
@@ -1,126 +1,85 @@
-function add(a, b) { // adds the two inputs.
-    return a + b;
-}
-function subtract(a, b) { // subtracts b from a
-    return a - b;
-}
-function multiplication(a, b) {
-    return a * b;
-}
-function division(a, b) {
-    if(b === 0){
-        return "Nice Try"
-    }else{
-    return a / b;
-    };
-}
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b !== 0 ? a / b : "Nice Try!"),
+};
 
-function operate(a, b, operator) { //calls the specified operator function and passes the two inputs into that func.
-    return operator(a,b);
+function operate(a, b, operator) {
+  // calls the specified operator function and passes the two inputs into that func.
+  return operator(a, b);
 }
 
 const display = document.querySelector("#display");
+
 let displayValue = "";
 let operator;
-const one = document.querySelector("#one-button");
-const two = document.querySelector("#two-button");
-const three = document.querySelector("#three-button");
-const four = document.querySelector("#four-button");
-const five = document.querySelector("#five-button");
-const six = document.querySelector("#six-button");
-const seven = document.querySelector("#seven-button");
-const eight = document.querySelector("#eight-button");
-const nine = document.querySelector("#nine-button");
-const zero = document.querySelector("#zero-button");
-const additionButton = document.querySelector("#addition-button");
-const subtractionButton = document.querySelector("#subtraction-button");
-const multiplicationButton = document.querySelector("#multiplication-button");
-const divisionButton = document.querySelector("#division-button");
+let firstOp;
+let secondOp;
+let final;
+
 const operatorButton = document.querySelector("#operator-button");
 const clearButton = document.querySelector("#clear-button");
 const backButton = document.querySelector("#back-button");
-clearButton.addEventListener("click", () => {
-    display.textContent = "";
-    displayValue = "";
-});
 
 function updateDisplay(int) {
-    displayValue += int
-
-    return display.textContent = displayValue;
+  displayValue += int;
+  display.textContent = displayValue;
 }
-one.addEventListener("click", () => {
-    updateDisplay(1);
-});
-two.addEventListener("click", () => {
-    updateDisplay(2);
-});
-three.addEventListener("click", () => {
-    updateDisplay(3);
-});
-four.addEventListener("click", () => {
-    updateDisplay(4);
-});
-five.addEventListener("click", () => {
-    updateDisplay(5);
-});
-six.addEventListener("click", () => {
-    updateDisplay(6);
-});
-seven.addEventListener("click", () => {
-    updateDisplay(7);
-});
-eight.addEventListener("click", () => {
-    updateDisplay(8);
-});
-nine.addEventListener("click", () => {
-    updateDisplay(9);
-});
-zero.addEventListener("click", () => {
-    updateDisplay(0);
-});
-additionButton.addEventListener("click", () => {
-    firstOp = parseInt(displayValue);
-    operator = add;
-    displayValue = "";
-    display.textContent = "";
-})
-operatorButton.addEventListener("click", () =>{
-    secondOp = parseInt(displayValue);
-   const answer = operate(firstOp, secondOp, operator);
-   displayValue = "";
-   final = Math.round((answer + Number.EPSILON) * 100) / 100
-   if(answer === "Nice Try"){
+
+[
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+].map((number) =>
+  document
+    .querySelector(`#${number}-button`)
+    .addEventListener("click", (el) => updateDisplay(el.target.innerHTML))
+);
+
+[
+  { elementName: "addition", operator: operations.add },
+  { elementName: "subtraction", operator: operations.subtract },
+  { elementName: "multiplication", operator: operations.multiply },
+  { elementName: "division", operator: operations.divide },
+].map((x) =>
+  document
+    .querySelector(`#${x.elementName}-button`)
+    .addEventListener("click", () => {
+      firstOp = parseInt(displayValue, 10);
+      operator = x.operator;
+      displayValue = "";
+      display.textContent = "";
+    })
+);
+
+operatorButton.addEventListener("click", () => {
+  secondOp = parseInt(displayValue, 10);
+  const answer = operate(firstOp, secondOp, operator);
+  displayValue = "";
+  final = Math.round((answer + Number.EPSILON) * 100) / 100;
+  if (answer === "Nice Try") {
     updateDisplay(answer);
-   }
-   updateDisplay(final);
-})
-subtractionButton.addEventListener("click", () => {
-    firstOp = parseInt(displayValue);
-    operator = subtract;
-    displayValue = "";
+  }
+  updateDisplay(final);
+});
 
-})
-multiplicationButton.addEventListener("click", () => {
-    firstOp = parseInt(displayValue);
-    operator = multiplication;
-    displayValue = "";
-
-})
-divisionButton.addEventListener("click", () => {
-    firstOp = parseInt(displayValue);
-    operator = division;
-    displayValue = "";
-
-})
 backButton.addEventListener("click", () => {
-let arr = Array.from(displayValue);
-arr.pop();
-let newDisplayValue = arr.join("");
-display.textContent = newDisplayValue;
-displayValue = newDisplayValue;
-return 
-}) // 
+  const arr = Array.from(displayValue);
+  arr.pop();
+  const newDisplayValue = arr.join("");
+  display.textContent = newDisplayValue;
+  displayValue = newDisplayValue;
+});
 
-
-
+clearButton.addEventListener("click", () => {
+  display.textContent = "";
+  displayValue = "";
+});
